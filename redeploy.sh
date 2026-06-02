7ea34b9705ebbd097c3824cc370a286c3a7ac7bb
smichalove <treven@Stevens-Air-2.lan>
2026-06-01 17:12:05 -0700

    Update redeploy.sh to copy and deploy snr_analysis.py to the Jetson Orin Nano edge server

--- redeploy.sh
+++ redeploy.sh
@@ -13,8 +13,8 @@ scp scratch/combined_logos_small.png steven@rainforestpi:~/rainforest-emu2-grid-
 scp dashboard.py README.md run_dashboard_system.sh steven@rainforestpi:~/rainforest-emu2-grid-dashboard/
 
 echo "=== Copying AI staging code and prompt to the Jetson Orin Nano ==="
-scp stage_local_summary.py stage_batch_summary.py gemma_hybrid_prompt.txt gemma_dft_prompt.txt steven@192.168.8.68:~/rainforest-emu2-grid-dashboard/
-ssh steven@192.168.8.68 "sudo cp ~/rainforest-emu2-grid-dashboard/stage_local_summary.py /home/grid_backup/ && sudo cp ~/rainforest-emu2-grid-dashboard/gemma_hybrid_prompt.txt /home/grid_backup/ && sudo cp ~/rainforest-emu2-grid-dashboard/gemma_dft_prompt.txt /home/grid_backup/ && sudo chown grid_backup:grid_backup /home/grid_backup/stage_local_summary.py /home/grid_backup/gemma_hybrid_prompt.txt /home/grid_backup/gemma_dft_prompt.txt && sudo systemctl restart jetson-grid-edge"
+scp stage_local_summary.py stage_batch_summary.py snr_analysis.py gemma_hybrid_prompt.txt gemma_dft_prompt.txt steven@192.168.8.68:~/rainforest-emu2-grid-dashboard/
+ssh steven@192.168.8.68 "sudo cp ~/rainforest-emu2-grid-dashboard/stage_local_summary.py /home/grid_backup/ && sudo cp ~/rainforest-emu2-grid-dashboard/snr_analysis.py /home/grid_backup/ && sudo cp ~/rainforest-emu2-grid-dashboard/gemma_hybrid_prompt.txt /home/grid_backup/ && sudo cp ~/rainforest-emu2-grid-dashboard/gemma_dft_prompt.txt /home/grid_backup/ && sudo chown grid_backup:grid_backup /home/grid_backup/stage_local_summary.py /home/grid_backup/snr_analysis.py /home/grid_backup/gemma_hybrid_prompt.txt /home/grid_backup/gemma_dft_prompt.txt && sudo systemctl restart jetson-grid-edge"
 
 
 
